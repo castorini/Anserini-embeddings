a51df752051d4666eb20176ff3b21b47bfd8418e
lintool <jimmylin@uwaterloo.ca>
2018-11-29 21:09:55 -0500

    wip

--- src/main/java/io/anserini/embeddings/IndexGloVe.java
+++ src/main/java/io/anserini/embeddings/IndexGloVe.java
@@ -19,11 +19,11 @@ package io.anserini.embeddings;
 import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.SimpleAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StoredField;
-import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
@@ -32,16 +32,17 @@ import org.apache.lucene.store.FSDirectory;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
 import org.deeplearning4j.models.word2vec.wordstore.VocabCache;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.OptionHandlerFilter;
+import org.kohsuke.args4j.ParserProperties;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -60,29 +61,29 @@ public class IndexGloVe {
   public static final String FIELD_VECTOR = "vector";
 
   public static void main(String[] args) throws Exception {
-//    Args indexArgs = new Args();
-//    CmdLineParser parser = new CmdLineParser(indexArgs, ParserProperties.defaults().withUsageWidth(90));
-//
-//    try {
-//      parser.parseArgument(args);
-//    } catch (CmdLineException e) {
-//      System.err.println(e.getMessage());
-//      parser.printUsage(System.err);
-//      System.err.println("Example: "+ IndexGloVe.class.getSimpleName() +
-//          parser.printExample(OptionHandlerFilter.REQUIRED));
-//      return;
-//    }
+    Args indexArgs = new Args();
+    CmdLineParser parser = new CmdLineParser(indexArgs, ParserProperties.defaults().withUsageWidth(90));
+
+    try {
+      parser.parseArgument(args);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      System.err.println("Example: "+ IndexGloVe.class.getSimpleName() +
+          parser.printExample(OptionHandlerFilter.REQUIRED));
+      return;
+    }
 
     long startTime = System.currentTimeMillis();
     LOG.info("Loading GloVe vectors...");
-    WordVectors wordVectors = WordVectorSerializer.loadTxtVectors(new File("../Castor-data/embeddings/GloVe/glove.840B.300d.short.txt"));
+    WordVectors wordVectors = WordVectorSerializer.loadTxtVectors(indexArgs.input);
     LOG.info("Completed in " + (System.currentTimeMillis()-startTime)/1000 + "s elapsed.");
 
     final long start = System.nanoTime();
     LOG.info("Starting indexer...");
 
-    final Directory dir = FSDirectory.open(Paths.get("glove-index"));
-    final SimpleAnalyzer analyzer = new SimpleAnalyzer();
+    final Directory dir = FSDirectory.open(indexArgs.index);
+    final Analyzer analyzer = new EnglishStemmingAnalyzer("porter"); // Default used in indexing.
     final IndexWriterConfig config = new IndexWriterConfig(analyzer);
     config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
 
@@ -100,7 +101,7 @@ public class IndexGloVe {
         DataOutputStream dataOut = new DataOutputStream(bytesOut);
         dataOut.writeInt(vector.length);
         for (int i = 0; i < vector.length; i++) {
-          dataOut.writeDouble(vector[i]);
+          dataOut.writeFloat((float) vector[i]);
         }
         dataOut.close();
       } catch (IOException e) {
@@ -134,9 +135,8 @@ public class IndexGloVe {
     }
 
     long duration = TimeUnit.MILLISECONDS.convert(System.nanoTime() - start, TimeUnit.NANOSECONDS);
-    LOG.info("Total " + numIndexed + " documents indexed in " +
+    LOG.info("Total " + numIndexed + " words indexed in " +
         DurationFormatUtils.formatDuration(duration, "HH:mm:ss"));
 
   }
-
 }
--- src/main/java/io/anserini/embeddings/LookupGlove.java
+++ src/main/java/io/anserini/embeddings/LookupGlove.java
@@ -17,6 +17,7 @@
 package io.anserini.embeddings;
 
 import io.anserini.util.AnalyzerUtils;
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.SimpleAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
@@ -26,27 +27,61 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.OptionHandlerFilter;
+import org.kohsuke.args4j.ParserProperties;
 
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
+import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
 public class LookupGlove {
-  public static void main(String[] args) throws Exception {
-    IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get("glove-index")));
+  public static final class Args {
+    @Option(name = "-word", metaVar = "[word]", required = true, usage = "word to look up")
+    public String word;
 
+    @Option(name = "-index", metaVar = "[path]", required = true, usage = "index path")
+    public Path index;
+  }
+
+  public static void main(String[] args) throws Exception {
+    Args lookupArgs = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(90));
+
+    try {
+      parser.parseArgument(args);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      System.err.println("Example: "+ LookupGlove.class.getSimpleName() +
+          parser.printExample(OptionHandlerFilter.REQUIRED));
+      return;
+    }
+
+    IndexReader reader = DirectoryReader.open(FSDirectory.open(lookupArgs.index));
     IndexSearcher searcher = new IndexSearcher(reader);
 
-    // cat
-    TermQuery query = new TermQuery(new Term(IndexGloVe.FIELD_WORD, "happy"));
+    Analyzer analyzer = new EnglishStemmingAnalyzer("porter"); // Default used in indexing.
+    List<String> qtokens = AnalyzerUtils.tokenize(analyzer, lookupArgs.word);
+    if (qtokens.size() != 1) {
+      System.err.println("Error: word tokenizes to more than one token");
+      System.exit(-1);
+    }
+
+    TermQuery query = new TermQuery(new Term(IndexGloVe.FIELD_WORD, qtokens.get(0)));
 
     TopDocs topDocs = searcher.search(query, Integer.MAX_VALUE);
     if (topDocs.totalHits == 0) {
       System.err.println("Error: term not found!");
-      return;// null;
+      System.exit(-1);
     }
+
 
     for ( int i=0; i<topDocs.scoreDocs.length; i++ ) {
       Document doc = reader.document(topDocs.scoreDocs[i].doc);
@@ -57,10 +92,10 @@ public class LookupGlove {
       int cnt = in.readInt();
       double[] vector = new double[cnt];
       for (int n=0; n<vector.length; n++) {
-        vector[n] = in.readDouble();
+        vector[n] = in.readFloat();
       }
 
-      System.out.println(tokens.size() + " " + doc.getField(IndexGloVe.FIELD_WORD).stringValue() + " " +
+      System.out.println(doc.getField(IndexGloVe.FIELD_WORD).stringValue() + " " + tokens.size() + " " +
           Arrays.toString(vector));
     }
   }
